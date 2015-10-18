c37fbd88f135a48f598a63ddaaf07fad17aaad4a
Max Ogden <max@maxogden.com>
2015-10-18 13:25:10 -0700

    use readable event

--- test-client.js
+++ test-client.js
@@ -9,21 +9,19 @@ var dg = DAG(db)
 var testServer = 'http://localhost:8080'
 
 var diff = dg.createDiffStream({binary: true})
-var ended = false
-diff.on('end', function () { ended = true })
+
+diff.on('end', function () {
+  console.log('since', diff.since)
+})
 
 getDiff()
 
 function getDiff () {
-  if (ended) {
-    return console.log('since:', diff.since)
-  }
-  diff.once('data', function (buff) {
-    request.post(testServer + '/diff', {body: buff, encoding: null}, function (err, res, data) {
-      console.log('POST /diff response:', data.length, 'bytes')
-      diff.write(data)
-      process.nextTick(getDiff)
-    })
+  var buff = diff.read()
+  if (!buff) return diff.once('readable', getDiff)
+  request.post(testServer + '/diff', {body: buff, encoding: null}, function (err, res, data) {
+    console.log('POST /diff response:', data.length, 'bytes')
+    diff.write(data)
+    process.nextTick(getDiff)
   })
 }
-
